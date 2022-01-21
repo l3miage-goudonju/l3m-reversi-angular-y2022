7e1e191e6a8fbe530a017c3386142d1be1428981
Goudon Justin <justin.goudon@outlook.fr>
2022-01-21 14:34:52 +0100

    Changed things on AI choosing where to play, seems to work fine when i can play, bcose still an error on game.turn condition

--- src/app/ia.service.ts
+++ src/app/ia.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { ReversiGameEngineService } from './reversi-game-engine.service';
 import { delay, Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 import { TileCoords } from './ReversiDefinitions';
 
 @Injectable({
@@ -8,40 +9,63 @@ import { TileCoords } from './ReversiDefinitions';
 })
 export class IaService {
 
-  /*constructor(public RGS: ReversiGameEngineService) { CODE EXPERIMENTAL, ESSAYE DE JOUER LE "MEILLEUR" COUP A CHAQUE FOIS. NE FONCTIONNE PAS
+  constructor(public RGS: ReversiGameEngineService) { /*CODE EXPERIMENTAL, ESSAYE DE JOUER LE "MEILLEUR" COUP A CHAQUE FOIS. 
+  ON RECUPERE LE COUP QUI DONNE LE PLUS DE TUILES ET ON LE JOUE. FONCTIONNE PARTIELLEMENT*/
     console.log("IA crée");
-    RGS.gameStateObs.subscribe((game) => {
+    RGS.gameStateObs.pipe(filter(game => game.turn !== 'Player1'), // à voir pour le filter et l'égalité, pas trop compris comment ça marche 
+    delay(2000)).subscribe(_ => {
       RGS.résuméDebug();
       const jePeuxJouerLa = RGS.whereCanPlay();
-      if (jePeuxJouerLa.length > 0) {
-        let plusDeTilesPrise: number;
-        let jeJoueIci: TileCoords = jePeuxJouerLa[0]; //  a voir, peut être à changer
-        jePeuxJouerLa.forEach(x => {
-          if (plusDeTilesPrise < RGS.PionsTakenIfPlayAt(x[0], x[1]).length) {
-            plusDeTilesPrise = RGS.PionsTakenIfPlayAt(x[0], x[1]).length;
-            jeJoueIci = x;
+      const nbCoupsPossibles = jePeuxJouerLa.length;
+      let plusDeTilesPrise: number = 0;
+      let jeJoueIci:number = 0;
+      if (nbCoupsPossibles > 0) {
+        for(let i:number = 0; i< nbCoupsPossibles; i++){
+          if (plusDeTilesPrise < RGS.PionsTakenIfPlayAt(jePeuxJouerLa[i][0], jePeuxJouerLa[i][1]).length) {
+            plusDeTilesPrise = RGS.PionsTakenIfPlayAt(jePeuxJouerLa[i][0], jePeuxJouerLa[i][1]).length;
+            jeJoueIci = i;
           }
-        });
-        RGS.play(jeJoueIci[0],jeJoueIci[1]);
+        }
       } else {
         console.log("Aucun coup possible pour l'ia");
       }
+      RGS.play(jePeuxJouerLa[jeJoueIci][0],jePeuxJouerLa[jeJoueIci][1]);
     });
-  }*/
-  constructor(public RGS: ReversiGameEngineService) {
+  }
+  /*constructor(public RGS: ReversiGameEngineService) {
     console.log("IA crée");
-    RGS.gameStateObs.pipe(delay(5000)).subscribe((game) => {
-      RGS.résuméDebug();
-      //if(game.turn === 'Player2'){
+    RGS.gameStateObs.pipe(filter(game => game.turn === 'Player2'),
+      delay(2000)).subscribe(_ => {
+        RGS.résuméDebug();
         const jePeuxJouerLa = RGS.whereCanPlay();
-        const nombreCoupsPossibles:number = jePeuxJouerLa.length;
-        if(nombreCoupsPossibles>0){
+        const nombreCoupsPossibles: number = jePeuxJouerLa.length;
+        if (nombreCoupsPossibles > 0) {
           const jeJoueIci = Math.floor(Math.random() * nombreCoupsPossibles);
-          RGS.play(jePeuxJouerLa[jeJoueIci][0],jePeuxJouerLa[jeJoueIci][0]);
+          RGS.play(jePeuxJouerLa[jeJoueIci][0], jePeuxJouerLa[jeJoueIci][1]);
         } else {
           console.log("Plus de coups possibles pour l'ia");
         }
-     // }
-    });
-  }
+      });
+  }fonction justin, ne fonctionne pas */
+
+  /*
+  constructor(public RGS: ReversiGameEngineService) {
+    const sub = RGS.gameStateObs
+      .pipe(
+        filter(game => game.turn!=="Player2"),
+        delay(3000)
+      )
+      .subscribe( _ => {
+        RGS.résuméDebug();
+        const possiblePlays = RGS.whereCanPlay();
+        if (possiblePlays.length > 0) {
+          const play = Math.floor(Math.random() * possiblePlays.length);
+          RGS.play(possiblePlays[play][0], possiblePlays[play][1]);
+        } else {
+          console.log("Aucun coup possible pour l'ia");
+        }
+      });
+
+    console.log('IA instanciée');
+  } fonction jérémie, ne fonctionne pas */
 }
